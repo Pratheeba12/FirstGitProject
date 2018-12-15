78811f7c1f05620b3551aad01214b73597cdda34
Pratheeba12 <pratheeb12@gmail.com>
2018-12-15 16:18:58 +0530

    ssss

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class PrintHelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.print("Hello Universe");
+		System.out.print("Happy");
+	}
+
+}
